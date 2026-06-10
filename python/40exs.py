48205be9934f961f97d214e16e6991ab1ddbfbac
Antoshuk Alina <antoshukalina@gmail.com>
2026-06-11 02:18:16 +0300

    1

--- python/40exs.py
+++ python/40exs.py
@@ -1,4 +1,4 @@
-#Write a Python function that accepts two integer numbers. 
+#1.Write a Python function that accepts two integer numbers. 
 #If the product of the two numbers is less than or equal to 1000
 # return their product; otherwise, return their sum.
 
